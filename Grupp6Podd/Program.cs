836c1c70d7256a29377834c54aa4c5558ca4c514
Sergeymeerk <kirgisv@live.com>
2025-11-04 16:15:37 +0100

    gg

--- Grupp6Podd/Program.cs
+++ Grupp6Podd/Program.cs
@@ -12,6 +12,7 @@ namespace Grupp6Podd
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            //Test2
         }
     }
 }
