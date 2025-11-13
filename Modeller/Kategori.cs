ff31312915960aaf9f9f34d9f66f624e47cea990
ellenscholdborg <ellish100@hotmail.com>
2025-11-13 16:20:55 +0100

    ändringar

--- Modeller/Kategori.cs
+++ Modeller/Kategori.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace Modeller
 {
     public class Kategori
     {
+        [BsonId] // anger att detta är dokumentents unika _id
+        public int Id { get; set; }
+
+        [BsonElement("Namn")]
+        public string Namn { get; set; }
+
+        public Kategori(int nyttId, string NyttNamn)
+        {
+            Id = nyttId;
+            Namn = NyttNamn;
+        }
+
     }
 }
+
